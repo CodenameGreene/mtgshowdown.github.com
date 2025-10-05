41185a5b6ba533d3f0b9bf0c00d72b424875e0ae
CodenameGreene <72770355+CodenameGreene@users.noreply.github.com>
2025-10-04 23:50:54 -0500

    Update MTGShowdown.js

--- MTGShowdown.js
+++ MTGShowdown.js
@@ -14,6 +14,7 @@ let player = {
   deck: [],
   hand: [],
   battlefield: [],
+  graveyard = [],
   landsPlayed: 0,
   turn: 1,
   // manaPool will be initialized when game starts
@@ -23,6 +24,7 @@ let opponent = {
   deck: [],
   hand: [],
   battlefield: [],
+  graveyard = [],
   landsPlayed: 0,
   turn: 1,
   manaPool: { W:0, U:0, B:0, R:0, G:0, C:0 }
