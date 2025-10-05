99b93e31af3e893c37aa34109e4d91eefe9af0f2
CodenameGreene <72770355+CodenameGreene@users.noreply.github.com>
2025-10-04 20:49:42 -0500

    Update MTGShowdown.js

--- MTGShowdown.js
+++ MTGShowdown.js
@@ -483,7 +483,15 @@ async function startGame() {
     fullDeck = await ensureTypeLines(fullDeck);
 
     // Shuffle deck
-    fullDeck.sort(() => Math.random() - 0.5);
+    function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+    }
+
+   fullDeck = shuffle(fullDeck);
 
     player.deck = fullDeck;
     player.hand = player.deck.splice(0, 7);
